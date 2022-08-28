257e9672ee73923490b192e4cb65a1105cdd8177
Monjur Ahmed Akash <monjur3069@gmail.com>
2022-08-28 21:41:13 +0600

    update
    Fetching data from api with image bug

--- lib/example2.dart
+++ lib/example2.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 
 class SecondExample extends StatefulWidget {
   static const String routeName = '/secondexample';
+
   const SecondExample({Key? key}) : super(key: key);
 
   @override
@@ -13,54 +14,57 @@ class SecondExample extends StatefulWidget {
 
 class _SecondExampleState extends State<SecondExample> {
   List<PhotosModel> photosList = [];
-  
-  Future<List<PhotosModel>> getPostsPhotos() async{
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+  Future<List<PhotosModel>> getPostsPhotos() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
 
     var data = jsonDecode(response.body.toString());
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       photosList.clear();
-      for(Map i in data){
-        PhotosModel photos = PhotosModel(title: i['title'], url: i['url'], id: i['id']);
+      for (Map i in data) {
+        PhotosModel photos =
+            PhotosModel(title: i['title'], url: i['url'], id: i['id']);
         photosList.add(photos);
       }
       return photosList;
-    }
-    else{
+    } else {
       return photosList;
     }
-
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Method'),
+        title: Text('Second Method fetch data'),
       ),
       body: FutureBuilder(
         future: getPostsPhotos(),
-        builder: (context,AsyncSnapshot<List<PhotosModel>> snapshot){
-          if(snapshot.hasData){
+        builder: (context, AsyncSnapshot<List<PhotosModel>> snapshot) {
+          if (snapshot.hasData) {
             return ListView.builder(
                 itemCount: photosList.length,
-                itemBuilder: (context,index){
+                itemBuilder: (context, index) {
                   return Column(
                     children: [
                       ListTile(
                         leading: CircleAvatar(
-                          //backgroundImage: NetworkImage(snapshot.data![index].url.toString()),
-                        ),
-                        title: Text(snapshot.data![index].id.toString()),//1st way to get data
-                        subtitle: Text(photosList[index].title.toString()),//second way to get data
+                  backgroundImage: NetworkImage('https://via.placeholder.com/150/92c952'),
+                  ),
+
+                        title: Text(snapshot.data![index].id.toString()),
+                        //1st way to get data
+                        subtitle: Text(photosList[index]
+                            .title
+                            .toString()), //second way to get data
                       ),
                     ],
                   );
                 });
-          }
-          else{
-            return Text('Loading...');
+          } else {
+            return Center(child: CircularProgressIndicator(),);
           }
         },
       ),
@@ -72,10 +76,5 @@ class PhotosModel {
   String title, url;
   num id;
 
-  PhotosModel({
-    required this.title,
-    required this.url,
-    required this.id
-  }
-  );
+  PhotosModel({required this.title, required this.url, required this.id});
 }
